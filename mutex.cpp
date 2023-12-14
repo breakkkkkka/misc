e839b1b0fcd9b261b218f5735bf89fd5efe74f5c
breakkkkkka <36813513+breakkkkkka@users.noreply.github.com>
2023-12-14 19:09:46 +0800

    Add files via upload

---
+++ mutex.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <Windows.h>
+#include <stdio.h>
+bool CheckProg()
+{
+
+	HANDLE  g_Handle = CreateMutex(NULL, FALSE, L"hello");
+	BOOL Reset = GetLastError();
+
+	if (g_Handle)
+	{
+		if (Reset == ERROR_ALREADY_EXISTS)
+		{
+
+			return true;
+		}
+
+	}
+	
+	
+		return false;
+	
+
+}
+int main()
+{
+
+
+	if(CheckProg())
+	{
+		return 0;
+	}
+	
+
+	getchar();
+
+
+}
